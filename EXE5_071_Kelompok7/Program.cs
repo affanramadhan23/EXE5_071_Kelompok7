d407288642b8ac64dd4c88fb9c4f790a8505b421
affanramadhan23 <affannabilramadhan@gmail.com>
2022-12-23 15:52:42 +0700

    Add project files.

---
+++ EXE5_071_Kelompok7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_071_Kelompok7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
