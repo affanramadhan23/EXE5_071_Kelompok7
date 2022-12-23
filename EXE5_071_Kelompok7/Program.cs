4e10ff81e07cf2567c28daaac434d3e6dfd84b53
affanramadhan23 <affannabilramadhan@gmail.com>
2022-12-23 16:10:46 +0700

    add class Node & class QueueLinkedList

--- EXE5_071_Kelompok7/Program.cs
+++ EXE5_071_Kelompok7/Program.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace EXE5_071_Kelompok7
 {
+    class Node
+    {
+        public string data;
+        public Node;
+    }  
+
+    class QueueLinkedList
+    {
+        Node Affan;
+        Node Ramadhan;
+
+        public QueueLinkedList()
+        {
+            Affan = null;
+            Ramadhan = null;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
